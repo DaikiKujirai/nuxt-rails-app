e194121f14ee306606f3c4bcb08cf3be90fed1a1
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-07-23 15:01:41 +0900

    [add] email_validation

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  before_validation :downcase_email
+
   has_secure_password
 
   validates :name,  presence: true,
                     length: { maximum: 30, allow_blank: true }
+  validates :email, presence: true,
+                    email: { allow_blank: true }
   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
   validates :password,  presence: true,
                         length: { minimum: 8 },
@@ -11,4 +15,26 @@ class User < ApplicationRecord
                           message: :invalid_password
                         },
                         allow_nil: true
+
+  ## methods
+  # class method  ###########################
+  class << self
+    # emailからアクティブなユーザーを返す
+    def find_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+  # class method end #########################
+
+  # 自分以外の同じemailのアクティブなユーザーがいる場合にtrueを返す
+  def email_activated?
+    users = User.where.not(id: id)
+    users.find_activated(email).present?
+  end
+
+  private
+
+  def downcase_email
+    self.email.downcase! if email
+  end
 end
