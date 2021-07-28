be1487c000f37070c5f9abf90b8596725eb9cfcf
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-07-28 13:24:16 +0900

    [add] login.vue

---
+++ front/pages/login.vue
@@ -0,0 +1,69 @@
+<template>
+  <bef-login-form-card #form-card-content> <!--eslint-disable-line-->
+    <v-form
+      ref="form"
+      v-model="isValid"
+    >
+      <user-form-email
+        :email.sync="params.auth.email"
+        no-validation
+      />
+      <user-form-password
+        :password.sync="params.auth.password"
+        no-validation
+      />
+      <v-card-actions>
+        <nuxt-link
+          to="#"
+          class="body-2 text-decoration-none"
+        >
+          パスワードを忘れた?
+        </nuxt-link>
+      </v-card-actions>
+      <v-card-text class="px-0">
+        <v-btn
+          :disabled="!isValid || loading"
+          :loading="loading"
+          block
+          color="success"
+          class="white--text"
+          @click="login"
+        >
+          ログインする
+        </v-btn>
+      </v-card-text>
+    </v-form>
+  </bef-login-form-card>
+</template>
+
+<script>
+import BefLoginFormCard from '../components/beforeLogin/befLoginFormCard.vue'
+import userFormEmail from '../components/user/userFormEmail.vue'
+import UserFormPassword from '../components/user/userFormPassword.vue'
+
+export default {
+  components: {
+    userFormEmail,
+    UserFormPassword,
+    BefLoginFormCard
+  },
+  layout: 'beforeLogin',
+  data () {
+    return {
+      isValid: false,
+      loading: false,
+      params: { auth: { email: '', password: '' } }
+    }
+  },
+  methods: {
+    login () {
+      this.loading = true
+      setTimeout(() => {
+        this.$store.dispatch('login')
+        this.$router.replace('/')
+        this.loading = false
+      }, 1500)
+    }
+  }
+}
+</script>
