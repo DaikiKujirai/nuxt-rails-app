47603256c09edd671d25377065805199f3cffce4
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-09-09 16:49:08 +0900

    [fix] edit_profile

--- front/components/post/InputFileForm.vue
+++ front/components/post/InputFileForm.vue
@@ -26,7 +26,7 @@ export default {
   },
   methods: {
     // 画像を選択時に画像データをエンコード //
-    async setImage () {
+    setImage () {
       const pageEdit = this.$route.name === 'users-edit'
       const typeChange = event.type === 'change'
       const labelAvatar = this.$options.propsData.label === 'プロフィール画像を選択してください'
@@ -50,59 +50,8 @@ export default {
         this.setImageInPreview(this.currentUser.cover_image.url)
         return
       }
-      const images = event.target.files || event.dataTransfer.files
       this.setImageInPostImage(event.target.files[0])
-      await this.getBase64(images[0])
-        .then((res) => {
-          const originalImg = new Image()
-          originalImg.src = res
-          originalImg.onload = () => {
-            const resizedCanvas = this.createResizedCanvasElement(originalImg)
-            const resizedBase64 = resizedCanvas.toDataURL(images[0].type)
-            this.setImageInPreview(resizedBase64)
-          }
-        })
-        .catch((err) => {
-          // eslint-disable-next-line no-console
-          console.error(err)
-        })
-    },
-    getBase64 (file) {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader()
-        reader.readAsDataURL(file)
-        reader.onload = () => resolve(reader.result)
-        reader.onerror = error => reject(error)
-      })
-    },
-    // 画像データをリサイズ //
-    createResizedCanvasElement (originalImg) {
-      const originalImgWidth = originalImg.width
-      const originalImgHeight = originalImg.height
-      const [resizedWidth, resizedHeight] = this.resizeWidthAndHeight(originalImgWidth, originalImgHeight)
-      const canvas = document.createElement('canvas')
-      const ctx = canvas.getContext('2d')
-      canvas.width = resizedWidth
-      canvas.height = resizedHeight
-      ctx.drawImage(originalImg, 0, 0, resizedWidth, resizedHeight)
-      return canvas
-    },
-    resizeWidthAndHeight (width, height) {
-      const MAX_HEIGHT = 400
-      const MAX_WIDTH = 400
-      if (width > height) {
-        if (width > MAX_WIDTH) {
-          height *= MAX_WIDTH / width
-          width = MAX_WIDTH
-        }
-      } else {
-        // eslint-disable-next-line no-lonely-if
-        if (height > MAX_HEIGHT) {
-          width *= MAX_HEIGHT / height
-          height = MAX_HEIGHT
-        }
-      }
-      return [width, height]
+      this.setImageInPreview(URL.createObjectURL(event.target.files[0]))
     },
     setImageInPreview (image) {
       this.$emit('setImageInPreview', image)
